97d6241b84dfa4427dd638fa3da4edb69013fc08
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2021-11-07 13:45:44 -0500

    Improve prescaling in sharp-bilinear shader

--- shaders/sharp-bilinear.fs
+++ shaders/sharp-bilinear.fs
@@ -15,7 +15,7 @@ in Vertex {
 out vec4 fragColor;
 
 void main() {
-float prescale = floor(targetSize.y / sourceSize[0].y);
+   float prescale = floor(targetSize.y / sourceSize[0].y + 0.01);
    vec2 texel = texCoord.xy * sourceSize[0].xy;
    vec2 texel_floored = floor(texel);
    vec2 s = fract(texel);
@@ -28,6 +28,6 @@ float prescale = floor(targetSize.y / sourceSize[0].y);
    vec2 f = (center_dist - clamp(center_dist, -region_range, region_range)) * prescale + 0.5;
 
    vec2 mod_texel = texel_floored + f;
-    
-   fragColor = texture(source[0], mod_texel / sourceSize[0].xy);;
+
+   fragColor = texture(source[0], mod_texel / sourceSize[0].xy);
 }
