1454c017e29822d71a9616c16109662ee48a242b
orbea <orbea@riseup.net>
2024-01-27 11:41:07 -0800

    mk: detect if miniz is vendored

--- mk/miniz.mk
+++ mk/miniz.mk
@@ -1,14 +1,20 @@
-USE_EXTERNAL_MINIZ ?= 0
+DIR_MINIZ := $(wildcard $(DEPDIR)/miniz)
+
+ifneq ($(DIR_MINIZ),)
+	USE_EXTERNAL_MINIZ ?= 0
+else
+	override USE_EXTERNAL_MINIZ := 1
+endif
 
 ifeq ($(USE_EXTERNAL_MINIZ), 0)
-	CFLAGS_MINIZ := -I$(DEPDIR)/miniz
+	CFLAGS_MINIZ := -I$(DIR_MINIZ)
 	LIBS_MINIZ :=
 	MKDIRS += deps/miniz
 	SRCS_MINIZ := deps/miniz/miniz.c
 	OBJS_MINIZ := $(SRCS_MINIZ:.c=.o)
 
 install-docs::
-	cp $(DEPDIR)/miniz/LICENSE $(DESTDIR)$(DOCDIR)/LICENSE-miniz
+	cp $(DIR_MINIZ)/LICENSE $(DESTDIR)$(DOCDIR)/LICENSE-miniz
 else
 	override REQUIRES_PRIVATE += miniz
 	CFLAGS_MINIZ = $(shell $(PKG_CONFIG) --cflags miniz)
